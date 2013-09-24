692d0462ac04354e76b5928b80872850b0790cf5
Adrian <adihg89@gmail.com>
2013-09-24 18:05:48 +0100

    Primeras pruebas. Esperando a que twitter me deje seguir

--- twitt.rb
+++ twitt.rb
@@ -13,6 +13,10 @@ puts
 
 tweet = Twitter.user_timeline(screen_name).first
 
+amigos = Twitter.friends(screen_name)
+amigos.each { |a| a_user2 = Twitter.user(a)
+				print "Username   : #{a_user2.name} \n"}
+
 if tweet
   puts "Tweet text : #{tweet.text }"
   puts "Tweet time : #{tweet.created_at}"
